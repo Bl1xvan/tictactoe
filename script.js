ba6cc1b34176bbdd11ee2119cd524ae15484a49b
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-01-02 19:29:55 -0500

    finally figured out the reverse trick!

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-const plyrbtns = Array.from(document.getElementsByClassName("plyrbtn"));
-
+const plyrbtns = Array.from(document.querySelectorAll(".plyrbtn"));
+const cellnumbers = Array.from(document.querySelectorAll("[data-cell]"));
+const overlay = document.getElementById("overlay");
+let currentPlayer
 plyrbtns.forEach(choosePlayer);
+cellnumbers.forEach(fillCell)
 
-let currentPlayer;
-function choosePlayer(plyrbtn){
+
+function choosePlayer(plyrbtn, key){
     plyrbtn.addEventListener("click", function(){
-        currentPlayer = plyrbtn.innerHTML
-        console.log(currentPlayer);
-    })
-    
+        currentPlayer = key;
+        overlay.style.display="none";
+    }) 
 }
+
+
+/////REVERSE CARD SUCCESS!!
+function fillCell(cellnumber){
+    cellnumber.addEventListener("click", function(){
+        console.log(currentPlayer);
+        cellnumber.innerText = plyrbtns[currentPlayer].innerText;
+        plyrbtns.reverse();
+    })
+}
+
+
+
+
